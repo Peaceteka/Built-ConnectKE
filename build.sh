2785ff3954ba4a39c9bb20273209926e5eb8f82d
Brian Peace Teka <peaceteka2@gmail.com>
2025-07-16 16:20:28 +0300

    Update build script with find/rsync for better file copying

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # Create public directory and copy all files
 mkdir -p public
-cp -r . public/
+# Copy all files except the public directory itself
+find . -maxdepth 1 -type d -not -name public -exec cp -r {} public/ \;
+find . -maxdepth 1 -type f -exec cp {} public/ \;
